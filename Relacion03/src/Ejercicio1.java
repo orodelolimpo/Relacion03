20dadf0392597e9c8cb05ae1f62974f7394eeb01
LYDIA <LYDIA@Lydia>
2022-12-20 12:44:07 +0100

    commit inicial

---
+++ Relacion03/src/Ejercicio1.java
@@ -0,0 +1,14 @@
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// 1. Algoritmo que imprima la tabla de multiplicar correspondiente a un número 
+		//comprendido entre uno y diez, introducido por el usuario, usando un esquema for. 
+		
+		Scanner teclado= new scanner
+		
+		
+
+	}
+
+}
